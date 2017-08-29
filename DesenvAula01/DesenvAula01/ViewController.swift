60c462a2aa48397414a8597d3bd725acf42ab1cc
Usuário Convidado <Guest@PA06MICRO14.local>
2017-08-28 22:09:59 -0300

    desenvAula01

--- DesenvAula01/DesenvAula01/ViewController.swift
+++ DesenvAula01/DesenvAula01/ViewController.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var tfNome: UITextField!
     @IBOutlet weak var tfEmail: UITextField!
     @IBOutlet weak var tfSenha: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tfNome.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -30,5 +32,19 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == tfNome{
+            tfEmail.becomeFirstResponder()
+        }else if textField == tfEmail{
+            tfPassword.becomeFirstResponder()
+        }else{
+            doSignUp()
+        }
+        return true
+    }
+    @IBAction func doSignUp() {
+        print("Fazendo o cadastro")
+        view.endEditing(true)
+    }
 }
 
